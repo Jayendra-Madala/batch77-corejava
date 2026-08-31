87e06b9658765cc317869070cc7d4b322fec97a8
Jayendra-Madala <jayendramadala8@gmail.com>
2026-08-31 14:26:48 +0530

    literals 1

---
+++ myfirstproject/src/com/languagefundamentals/TestLiteralsDemo1.java
@@ -0,0 +1,21 @@
+package com.languagefundamentals;
+
+public class TestLiteralsDemo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Main method started");
+		
+		int a1 = 123;
+		int a2 = 1230;
+		int a3 = 00;
+		int a4 = 0123;
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+			
+		
+		System.out.println("Main method ended");
+	}
+
+}
